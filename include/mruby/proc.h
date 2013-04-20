ed53d8a07278fb657c7601f0215fa235d19c4692
Masaki Muranaka <monaka@monami-ya.jp>
2013-04-21 02:35:02 +0900

    simplify aspec unpack macros.

--- include/mruby/proc.h
+++ include/mruby/proc.h
@@ -32,13 +32,13 @@ struct RProc {
 };
 
 /* aspec access */
-#define ARGS_GETREQ(a)          (((a) >> 19) & 0x1f)
-#define ARGS_GETOPT(a)          (((a) >> 14) & 0x1f)
-#define ARGS_GETREST(a)         ((a) & (1<<13))
-#define ARGS_GETPOST(a)         (((a) >> 8) & 0x1f)
-#define ARGS_GETKEY(a)          (((a) >> 3) & 0x1f))
-#define ARGS_GETKDICT(a)        ((a) & (1<<2))
-#define ARGS_GETBLOCK(a)        ((a) & (1<<1))
+#define ARGS_GETREQ(a)          (((a) >> 18) & 0x1f)
+#define ARGS_GETOPT(a)          (((a) >> 13) & 0x1f)
+#define ARGS_GETREST(a)         ((a) & (1 << 12))
+#define ARGS_GETPOST(a)         (((a) >> 7) & 0x1f)
+#define ARGS_GETKEY(a)          (((a) >> 2) & 0x1f))
+#define ARGS_GETKDICT(a)        ((a) & 2)
+#define ARGS_GETBLOCK(a)        ((a) & 1)
 
 #define MRB_PROC_CFUNC 128
 #define MRB_PROC_CFUNC_P(p) ((p)->flags & MRB_PROC_CFUNC)
