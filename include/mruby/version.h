4ea8d9be257ced7e60311dcc207f86be09d52881
Masaki Muranaka <monaka@monami-ya.jp>
2014-03-01 15:26:32 +0900

    Add information about monami-ya.mrb. This will avoid user's confusion about runtime behavior.

--- include/mruby/version.h
+++ include/mruby/version.h
@@ -2,7 +2,7 @@
 #define MRUBY_VERSION_H
 
 #define MRUBY_RUBY_VERSION "1.9"
-#define MRUBY_RUBY_ENGINE  "mruby"
+#define MRUBY_RUBY_ENGINE  "mruby (monami-ya.mrb)"
 
 #define MRB_STRINGIZE0(expr) #expr
 #define MRB_STRINGIZE(expr) MRB_STRINGIZE0(expr)
@@ -24,9 +24,12 @@
 
 #define MRUBY_AUTHOR "mruby developers"
 
+#define MONAMI_YA_MRB_RELEASE "1.0.1-SNAPSHOT"
+#define MONAMI_YA_MRB_RELEASE_DATE 2014
+#define MONAMI_YA_MRB_AUTHOR "monami-ya.mrb developers"
 
 #define MRUBY_DESCRIPTION      \
-  "mruby " MRUBY_VERSION       \
+  "monami-ya.mrb " MRUBY_VERSION       \
   " (" MRUBY_RELEASE_DATE ") " \
 
 #define MRUBY_COPYRIGHT                \
@@ -34,5 +37,9 @@
   MRB_STRINGIZE(MRUBY_BIRTH_YEAR)"-"   \
   MRB_STRINGIZE(MRUBY_RELEASE_YEAR)" " \
   MRUBY_AUTHOR                         \
+  "\nmonami-ya.mrb - Copyright (C) "           \
+  "2012-"                                      \
+  MRB_STRINGIZE(MONAMI_YA_MRB_RELEASE_YEAR) " "\
+  MONAMI_YA_MRB_AUTHOR
 
 #endif  /* MRUBY_VERSION_H */
