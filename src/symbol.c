78cee794b5d3ece49b387e541fa7d1ed28102814
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2014-03-15 15:43:40 +0900

    symbol length type to be mrb_int

--- src/symbol.c
+++ src/symbol.c
@@ -41,7 +41,7 @@ KHASH_DECLARE(n2s, symbol_name, mrb_sym, 1)
 KHASH_DEFINE (n2s, symbol_name, mrb_sym, 1, sym_hash_func, sym_hash_equal)
 /* ------------------------------------------------------ */
 static mrb_sym
-sym_intern(mrb_state *mrb, const char *name, size_t len, mrb_bool lit)
+sym_intern(mrb_state *mrb, const char *name, mrb_int len, mrb_bool lit)
 {
   khash_t(n2s) *h = mrb->name2sym;
   symbol_name sname;
@@ -76,13 +76,13 @@ sym_intern(mrb_state *mrb, const char *name, size_t len, mrb_bool lit)
 }
 
 mrb_sym
-mrb_intern(mrb_state *mrb, const char *name, size_t len)
+mrb_intern(mrb_state *mrb, const char *name, mrb_int len)
 {
   return sym_intern(mrb, name, len, FALSE);
 }
 
 mrb_sym
-mrb_intern_static(mrb_state *mrb, const char *name, size_t len)
+mrb_intern_static(mrb_state *mrb, const char *name, mrb_int len)
 {
   return sym_intern(mrb, name, len, TRUE);
 }
@@ -90,7 +90,7 @@ mrb_intern_static(mrb_state *mrb, const char *name, size_t len)
 mrb_sym
 mrb_intern_cstr(mrb_state *mrb, const char *name)
 {
-  return mrb_intern(mrb, name, strlen(name));
+  return mrb_intern(mrb, name, (mrb_int)strlen(name));
 }
 
 mrb_sym
@@ -100,7 +100,7 @@ mrb_intern_str(mrb_state *mrb, mrb_value str)
 }
 
 mrb_value
-mrb_check_intern(mrb_state *mrb, const char *name, size_t len)
+mrb_check_intern(mrb_state *mrb, const char *name, mrb_int len)
 {
   khash_t(n2s) *h = mrb->name2sym;
   symbol_name sname = { 0 };
@@ -122,7 +122,7 @@ mrb_check_intern(mrb_state *mrb, const char *name, size_t len)
 mrb_value
 mrb_check_intern_cstr(mrb_state *mrb, const char *name)
 {
-  return mrb_check_intern(mrb, name, strlen(name));
+  return mrb_check_intern(mrb, name, (mrb_int)strlen(name));
 }
 
 mrb_value
@@ -133,7 +133,7 @@ mrb_check_intern_str(mrb_state *mrb, mrb_value str)
 
 /* lenp must be a pointer to a size_t variable */
 const char*
-mrb_sym2name_len(mrb_state *mrb, mrb_sym sym, size_t *lenp)
+mrb_sym2name_len(mrb_state *mrb, mrb_sym sym, mrb_int *lenp)
 {
   khash_t(n2s) *h = mrb->name2sym;
   khiter_t k;
@@ -143,12 +143,12 @@ mrb_sym2name_len(mrb_state *mrb, mrb_sym sym, size_t *lenp)
     if (kh_exist(h, k)) {
       if (kh_value(h, k) == sym) {
         sname = kh_key(h, k);
-        *lenp = sname.len;
+        if (lenp) *lenp = sname.len;
         return sname.name;
       }
     }
   }
-  *lenp = 0;
+  if (lenp) *lenp = 0;
   return NULL;  /* missing */
 }
 
@@ -246,7 +246,7 @@ mrb_sym_to_s(mrb_state *mrb, mrb_value sym)
 {
   mrb_sym id = mrb_symbol(sym);
   const char *p;
-  size_t len;
+  mrb_int len;
 
   p = mrb_sym2name_len(mrb, id, &len);
   return mrb_str_new_static(mrb, p, len);
@@ -398,7 +398,7 @@ sym_inspect(mrb_state *mrb, mrb_value sym)
 {
   mrb_value str;
   const char *name;
-  size_t len;
+  mrb_int len;
   mrb_sym id = mrb_symbol(sym);
 
   name = mrb_sym2name_len(mrb, id, &len);
@@ -415,7 +415,7 @@ sym_inspect(mrb_state *mrb, mrb_value sym)
 mrb_value
 mrb_sym2str(mrb_state *mrb, mrb_sym sym)
 {
-  size_t len;
+  mrb_int len;
   const char *name = mrb_sym2name_len(mrb, sym, &len);
 
   if (!name) return mrb_undef_value(); /* can't happen */
@@ -425,11 +425,11 @@ mrb_sym2str(mrb_state *mrb, mrb_sym sym)
 const char*
 mrb_sym2name(mrb_state *mrb, mrb_sym sym)
 {
-  size_t len;
+  mrb_int len;
   const char *name = mrb_sym2name_len(mrb, sym, &len);
 
   if (!name) return NULL;
-  if (symname_p(name) && strlen(name) == len) {
+  if (symname_p(name) && strlen(name) == (size_t)len) {
     return name;
   }
   else {
@@ -454,7 +454,7 @@ sym_cmp(mrb_state *mrb, mrb_value s1)
   else {
     const char *p1, *p2;
     int retval;
-    size_t len, len1, len2;
+    mrb_int len, len1, len2;
 
     p1 = mrb_sym2name_len(mrb, sym1, &len1);
     p2 = mrb_sym2name_len(mrb, sym2, &len2);
